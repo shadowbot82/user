53b9f52c708300b6b4141d9a5446cc76dfc238e6
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-01-14 05:59:28 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,12 @@
+<?php
+/* setting bot */
+$baca_plus=$red."
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$red
+──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$red
+──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
+creator:$ijo adidoank$putih || code invite:$ijo F9NAmmRD7x$putih
+chanel :$kuning sungging$putih || code invite:$ijo F3NW34owm4".$t;
+$msg_baca= "[•] official chanel adi bordir";
+$stat_baca=true;
+ 
+?>
