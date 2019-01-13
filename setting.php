dd613021ba57163bc57bd5fc008e6141352625be
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-01-14 06:02:15 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,12 +1,12 @@
 <?php
 /* setting bot */
-$baca_plus=$red."
-──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$kuning █▀▄ █── █─█ ▄▀▀────$red
-──── █▀█ █▀█ █─ █▀█────$kuning █─█ █─▄ █─█ ─▀▄────$red
-──── ▀▀─ ▀─▀ ─▀ ▀─▀────$kuning █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
-creator:$ijo adidoank$putih || code invite:$ijo F9NAmmRD7x$putih
-chanel :$kuning sungging$putih || code invite:$ijo F3NW34owm4".$t;
-$msg_baca= "[•] official chanel adi bordir";
+$baca_plus=$ijo."
+──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$turkis █▀▄ █── █─█ ▄▀▀────$ijo
+──── █▀█ █▀█ █─ █▀█────$turkis █─█ █─▄ █─█ ─▀▄────$ijo
+──── ▀▀─ ▀─▀ ─▀ ▀─▀────$turkis █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
+creator:$ijo Shadow Bot$putih || code invite:$ijo F9NAmmRD7x".$t;
+
+$msg_baca= "[•] Subscribe Yt => SHADOW BOT CHANNEL ";
 $stat_baca=true;
  
 ?>
