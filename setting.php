3db9638e27cfd2f92b8e4781a509b1e65c86db60
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-01-15 11:41:12 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -4,7 +4,7 @@ $baca_plus=$ijo."
 ──── █▀▄ ▄▀▄ ▄▀ ▄▀▄────$turkis █▀▄ █── █─█ ▄▀▀────$ijo
 ──── █▀█ █▀█ █─ █▀█────$turkis █─█ █─▄ █─█ ─▀▄────$ijo
 ──── ▀▀─ ▀─▀ ─▀ ▀─▀────$turkis █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
-creator:$kuning Shadow Bot$putih || code invite:$kuning F1Y5t1lzGx".$t;
+creator:$kuning Aris Ahmad Risadi$putih || code invite:$kuning F1Y5t1lzGx".$t;
 
 $msg_baca=$ijo."[!] Note : ".$t."[•] Jika terjadi kesalahan".$t."[•] berarti os hp lu di bawah lolipop".$t."[•] jika mau work harus di atas lolipop".$t;
 $stat_baca=true;
