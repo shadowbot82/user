3c39a74f1fd2c71d596e66b1b89d34eb19bf5294
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-01-15 11:53:02 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -6,7 +6,7 @@ $baca_plus=$ijo."
 ──── ▀▀─ ▀─▀ ─▀ ▀─▀────$turkis █▀─ ▀▀▀ ─▀─ ▀▀─────$putih
 creator:$kuning Shadow Bot$putih || code invite:$kuning F1Y5t1lzGx".$t;
 
-$msg_baca=$ijo."[•] Subscribe my channel => SHADOW BOT CHANNEL".$t"[!] Note : ".$t."[•] Jika terjadi kesalahan".$t."[•] berarti os hp lu di bawah lolipop".$t."[•] jika mau work harus di atas lolipop".$t;
+$msg_baca=$ijo."[•] Subscribe my channel => SHADOW BOT CHANNEL".$t."[!] Note : ".$t."[•] Jika terjadi kesalahan".$t."[•] berarti os hp lu di bawah lolipop".$t."[•] jika mau work harus di atas lolipop".$t;
 $stat_baca=true;
  
 ?>
