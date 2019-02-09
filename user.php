811f7d35cf69ed8704f2875da91bcd6d1525d3e9
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-02-09 08:07:21 +0700

    Add files via upload

---
+++ user.php
@@ -0,0 +1,30 @@
+<?php
+echo $putih."[$red!$putih]$red Warning !!!".$t;
+echo $putih."[•] Ini adalah program ilegal".$t;
+echo $putih."[•] Resiko sepenuhnya di tanggung pengguna".$t;
+$msgwe=$putih."[".$ijo."•".$putih."] Script VIP+$red v.2.0$putih =>$red Yt$putih :$red eftechanel".$t;
+$msgve=$red."[•] Apk new pengganti Watch & earn".$t;
+$msgyd=$putih."[$red!$putih] Update [$red v.1.2$putih ] Premium & Multi Account".$t;
+$msgtt=$putih."[*] Enjoy Your Life !! ".$t;
+$msgads=$putih."[$biru>$putih] Subscribe Channel YT EfteChannel".$t;
+$msgssdoge=$putih."[^] Enjoy Your Life !! ".$t;
+$stat_dony=true;
+$st_dony=true;
+$yd_stat=true;
+$vip1=false;
+$weblock=[""];
+$ydblock=[""];
+$we_stat=true;
+$tt_stat=true;
+$stat_ads=true;
+$data= [
+//we
+'mumet banget','Sondong Jpr','Muna Sosweety','Rully Efte','dje fourten','0','Ahmad Sakhowi',
+        '1','2','3','4','5','6',
+//Yogo
+        'Rully Efte','Nadeeda Zavi',
+        ];
+$ydnews = ['tes','eftechanel',];
+$watch = ['tes',];
+$cword= ['tes','eftechanel',];
+$yogo= ['Rully Efte','Nadeeda Zavi','Abdu Fajar','mizan bagustian','Andy Ramdani',];
