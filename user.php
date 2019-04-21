9c4b413301a5067a0a04a639b0b1cff5277bee84
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-04-21 20:56:48 +0700

    Update user.php

--- user.php
+++ user.php
@@ -11,7 +11,7 @@ $msgyg=$ijo."[".$kuning.">".$ijo."] Official Yt SHADOW BOT CHANNEL";
 $msgads=$ijo."[".$kuning.">".$ijo."] Jangan Lupa Subscribe ";
 $msgadsy=$putih."[".$ijo."√".$putih."] Note : ".$t.$ijo."[". $putih."IND". $ijo."] Gunakan botnya jangan berlebihan takutnya nanti kena banned atau nanti gak bisa wd".$t."[".$putih."ENG".$ijo."] use bots don't overdo fear later you get banned or can't withdraw".$t;
 
-
+$sk="skidipap";
 
 
 $msgwe=$putih."[".$ijo."•".$putih."] Script VIP+$red v.2.0$putih =>$red Yt$putih :$red eftechanel".$t;
